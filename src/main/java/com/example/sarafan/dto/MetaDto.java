6ec655ddf4327d3585e25006b77fcc4e1d45f4ed
Awelless <hahaha.funny.yellow.dog@gmail.com>
2020-11-27 16:18:58 +0300

    nyaaan

---
+++ src/main/java/com/example/sarafan/dto/MetaDto.java
@@ -0,0 +1,12 @@
+package com.example.sarafan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MetaDto {
+    private String title;
+    private String description;
+    private String cover;
+}
